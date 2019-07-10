3ad802a5cb1fba4589acd240d39b46606f56dd63
SierraOG <sierrao@utexas.edu>
2019-07-10 15:00:30 -0500

    added comments to menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -29,29 +29,36 @@ let menuItems = [
 // select header because menu will be appended to header
 const header = document.querySelector('.header');
 
+// creates menu component using menuItems array and appends this after the header
+header.append(createMenu(menuItems));
+
 function createMenu(menuArray){
   // define new elements
   const menu = document.createElement('div')
   const menuUL = document.createElement('ul');
   const menuLIs = [];
 
+  // loops through array of menu items, creating a new list item for each one and appending that list item as a child to the unordered list element defined above
   menuArray.forEach(function(item, i) {
     menuLIs[i] = document.createElement('li');
     menuLIs[i].textContent = item;
     menuUL.appendChild(menuLIs[i]);
   })
 
+  // appends unordered list with list items to the menu component
   menu.appendChild(menuUL);
 
+  // add menu class to menu component
   menu.classList.add('menu');
 
+  // selects menu button on DOM
   const menuButton = document.querySelector('.menu-button');
 
+  // add click event to menu button to open and close the menu
   menuButton.addEventListener('click', event => {
     menu.classList.toggle('menu--open');
   })
 
+  // reurns menu component
   return menu;
 }
-
-header.append(createMenu(menuItems));
